31285643d7a45141db5c729985609738b01960c3
ethan <1127705669@qq.com>
2022-08-18 07:58:03 -0700

    feat(ROS) add ROS module
    1. add ROS.cc
    2. add ROS.h
    3. add Init.cc
    4. add Init.h
    
    Change-Id: I9938e856ea9b82ccf3a392771f68662311533be2

---
+++ ROS/ROS.cc
@@ -0,0 +1,7 @@
+
+
+namespace EDrive {
+namespace EROS{
+    
+}
+}
---
+++ ROS/ROS.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * Copyright 2022 The EDrive Authors. All Rights Reserved.
+ *
+ *****************************************************************************/
+
+#ifndef EDrive_ROS_H_
+#define EDrive_ROS_H_
+
+namespace EDrive {
+namespace EROS{
+    
+}
+}
+
+#endif
