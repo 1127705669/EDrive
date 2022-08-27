5e38ab3d06831c9013ddeec4574cf2f8fb1dfe51
ethan <1127705669@qq.com>
2022-08-27 18:03:39 +0800

    fix(EROS/base): add base not compiled bug
    Change-Id: I2fc8ab980af347207b29b7a1fb27375fc216d18f

--- EROS/build/cmake_install.cmake
+++ EROS/build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/ethan/Workset/EDrive/EROS/build/gtest/cmake_install.cmake")
+  include("/home/ethan/Workset/EDrive/EROS/build/base/cmake_install.cmake")
   include("/home/ethan/Workset/EDrive/EROS/build/node/cmake_install.cmake")
 
 endif()
