77a9e40260f5ce57750c5847da283ce1bba299be
ethan <1127705669@qq.com>
2022-08-27 18:04:14 +0800

    feat(app): add call EROS.launch file function

--- app/EDrive.cc
+++ app/EDrive.cc
@@ -2,9 +2,11 @@
 #include <iostream>
 
 #include "ros/ros.h"
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
+    execlp("roslaunch", "roslaunch", "EROS","EROS.launch", NULL);
     std::cout << "EDrive" << std::endl;
     return 0;
 }
