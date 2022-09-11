5c26d0fc7e40c5ca0f1ad319e138f5c54f701462
ethan <1127705669@qq.com>
2022-09-11 12:13:11 +0800

    feat(planning): add cartesian to frenet
    1. cartesian_frenet_conversion.cc
    2. cartesian_frenet_conversion.h
    3. pnc_point.msg
    
    Change-Id: I1f8bdca863303aceace19c9038ee56bab3f238fe

---
+++ modules/common/math/cartesian_frenet_conversion.cc
@@ -0,0 +1,33 @@
+/******************************************************************************
+ * Copyright 2022 The EDrive Authors. All Rights Reserved.
+ *****************************************************************************/
+
+#include "modules/common/math/cartesian_frenet_conversion.h"
+
+namespace EDrive {
+namespace common {
+namespace math {
+
+void CartesianFrenetConverter::cartesian_to_frenet() {
+
+}
+
+void CartesianFrenetConverter::frenet_to_cartesian() {
+
+}
+
+void CartesianFrenetConverter::frenet_to_cartesian() {
+
+}
+
+double CartesianFrenetConverter::CalculateTheta() {
+
+}
+
+double CartesianFrenetConverter::CalculateKappa() {
+    
+}
+
+} // math
+} // common
+} // EDrive
