3209a99b624ec4e2e2fbd0e36d325e9e7c3a9c39
ethan <1127705669@qq.com>
2022-10-02 02:00:18 +0800

    refactor(build): remove build file in EROS
    Change-Id: Id3d74ba019e4f74b6ffce6d63fe6b475afd1d6bc

--- EROS/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ethan/Workset/EDrive/EROS/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ethan/Workset/EDrive/EROS/devel;/opt/ros/melodic'.split(';') if '/home/ethan/Workset/EDrive/EROS/devel;/opt/ros/melodic' != '' else []
