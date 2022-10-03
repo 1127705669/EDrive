f0c927c36679063e07998c5334c9a21f670b950c
ethan <1127705669@qq.com>
2022-10-03 14:18:41 +0800

    feat(control): add pid_cotroller

---
+++ modules/control/common/pid_controller.cc
@@ -0,0 +1,17 @@
+/******************************************************************************
+ * Copyright 2022 The EDrive Authors. All Rights Reserved.
+ *****************************************************************************/
+
+#include "modules/control/common/pid_controller.h"
+
+#include <cmath>
+
+namespace EDrive {
+namespace control {
+
+void PIDController::Init(const PidConf &pid_conf) {
+
+}
+
+} // control
+} // EDrive
