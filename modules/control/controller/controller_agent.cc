9897b6af465ee517ec2d312c8d4f673b246ab65d
ethan <1127705669@qq.com>
2022-10-04 02:22:54 +0800

    feat(control): add controller agent
    Change-Id: I9b884910d00863e165a823794eaf71c685bd0029

---
+++ modules/control/controller/controller_agent.cc
@@ -0,0 +1,14 @@
+/******************************************************************************
+ * Copyright 2022 The EDrive Authors. All Rights Reserved.
+ *****************************************************************************/
+
+#include "modules/control/controller/controller_agent.h"
+
+#include "modules/control/controller/lat_controller.h"
+#include "modules/control/controller/lon_controller.h"
+
+namespace EDrive {
+namespace control {
+
+} // control
+} // EDrive
