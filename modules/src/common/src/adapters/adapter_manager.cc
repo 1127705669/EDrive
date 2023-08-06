be61cf1f6158b791ae221ea8073e63a751f1ff01
ethan <ethan.liqi.yi@gmail.com>
2023-08-06 15:57:28 +0800

    feat(common): establish util function
    Change-Id: I0e2ba9fadca74b1c9b76bc762c5a55b5da803dbb

--- modules/src/common/src/adapters/adapter_manager.cc
+++ modules/src/common/src/adapters/adapter_manager.cc
@@ -14,11 +14,23 @@ AdapterManager::AdapterManager() {}
 void AdapterManager::Init(const std::string &adapter_config_filename) {
   // Parse config file
   AdapterManagerConfig configs;
+  configs.add_config();
   Init(configs);
 }
 
 void AdapterManager::Init(const AdapterManagerConfig &configs) {
   ROS_INFO("adapter manager init");
+
+  for (const auto &config : configs.config()) {
+    switch (config.type()) {
+      case AdapterConfig::CONTROL_COMMAND:
+        EnableControlCommand("CONTROL", config);
+        break;
+      default:
+        ROS_INFO("Unknown adapter config type!");
+        break;
+    }
+  }
 }
 
 } // adapter
