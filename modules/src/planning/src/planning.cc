286149b6428bc49c253722f040469670c4a0eff9
ethan <ethan.liqi.yi@gmail.com>
2023-09-11 05:05:38 +0800

    feat(planning):add planning feature structure
    Change-Id: Id745fb4786c372749c9a6b63c28fbbe48478a104

--- modules/src/planning/src/planning.cc
+++ modules/src/planning/src/planning.cc
@@ -3,26 +3,58 @@
  *****************************************************************************/
 
 #include <ros/ros.h>
-#include "common/src/adapters/adapter_manager.h"
 #include "planning/src/planning.h"
+#include "common/src/util/file.h"
 
 namespace EDrive {
 namespace planning {
 
 using EDrive::Result_state;
+using ::planning::ADCTrajectory;
 using EDrive::common::adapter::AdapterManager;
 
-// Planning::~Planning() { Stop(); }
+Planning::~Planning() { Stop(); }
 
 std::string Planning::Name() const { return "planning"; }
 
-EDrive::Result_state Planning::Init(){
-  std::string planning_name = "/home/ethan/workset/EDrive/modules/src/planning/conf/adapter.conf";
-  AdapterManager::Init(planning_name);
+void Planning::PublishPlanningPb(ADCTrajectory* trajectory_pb) {
+  Publish(trajectory_pb);
+}
+
+Result_state Planning::RegisterPlanners() {
+
   return State_Ok;
 }
 
-EDrive::Result_state Planning::Start(){
+void Planning::RunOnce() {
+  Result_state state;
+
+  // snapshot all coming data
+  AdapterManager::Observe();
+  
+  state = RegisterPlanners();
+
+  ADCTrajectory* trajectory_pb;
+  trajectory_pb->total_path_length = 10.0;
+  PublishPlanningPb(trajectory_pb);
+}
+
+Result_state Planning::Init(){
+  ROS_INFO("Planning init, starting...");
+
+  root_path = EDrive::common::util::GetRootPath();
+  adapter_conf_file = root_path + adapter_conf_file;
+  planning_conf_file = root_path + planning_conf_file;
+
+  if (!AdapterManager::Initialized()) {
+    ROS_INFO("  registering node: %s", Name().c_str());
+    AdapterManager::Init(adapter_conf_file);
+  }
+
+  return State_Ok;
+}
+
+Result_state Planning::Start(){
   timer_ = EDrive::common::adapter::AdapterManager::CreateTimer(ros::Duration(planning_period), 
                                                                 &Planning::OnTimer,
                                                                 this);
@@ -35,6 +67,7 @@ void Planning::Stop() {
 
 void Planning::OnTimer(const ros::TimerEvent &) {
   ros::Time begin = ros::Time::now();
+  RunOnce();
 }
 
 } // planning
--- modules/src/planning/src/planning.h
+++ modules/src/planning/src/planning.h
@@ -7,15 +7,20 @@
 #include <string>
 #include "common/src/EDrive.h"
 #include "common/src/state.h"
+
+#include "common/src/adapters/adapter_manager.h"
+
 #include "planning/ADCTrajectory.h"
 
 namespace EDrive {
 namespace planning {
 
+using EDrive::common::adapter::AdapterManager;
+
 class Planning : public EDrive::common::EDriveApp {
  public:
-//   Planning() = default;
-//   virtual ~Planning();
+  Planning() = default;
+  virtual ~Planning();
 
   std::string Name() const override;
 
@@ -25,12 +30,34 @@ class Planning : public EDrive::common::EDriveApp {
 
   void Stop() override;
 
+  /**
+   * @brief main logic of the planning module, runs periodically triggered by
+   * timer.
+   */
+  void RunOnce();
+
+  /**
+   * @brief Fill the header and publish the planning message.
+   */
+  void Publish(::planning::ADCTrajectory* trajectory) {
+    
+    AdapterManager::PublishPlanning(*trajectory);
+  }
+
  private:
   // Watch dog timer
   void OnTimer(const ros::TimerEvent &);
+
+  EDrive::Result_state RegisterPlanners();
+
+  void PublishPlanningPb(::planning::ADCTrajectory* trajectory_pb);
   const double planning_period = 0.1;
   ros::Timer timer_;
+
+  std::string root_path;
+  std::string adapter_conf_file = "/src/planning/conf/adapter.conf";
+  std::string planning_conf_file = "/src/planning/conf/planning.conf";
 };
 
-} // planning
-} // EDrive
+} // namespace planning
+} // namespace EDrive
