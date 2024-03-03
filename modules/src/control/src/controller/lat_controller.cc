ae238ff8ca2bcfb101157b674ca064dda69addbf
ethan <ethan.liqi.yi@gmail.com>
2024-03-03 08:45:21 +0800

    feat(control): initialize lateral controller
    Change-Id: Iac7804464cecf9d74ff6dc12c17985bf8981cf7b

--- modules/src/control/src/controller/lat_controller.cc
+++ modules/src/control/src/controller/lat_controller.cc
@@ -9,10 +9,11 @@ namespace EDrive {
 namespace control {
 
 using EDrive::Result_state;
+using Matrix = Eigen::MatrixXd;
 
 constexpr double GRA_ACC = 9.8;
 
-LatController::LatController() : name_("LatController"){
+LatController::LatController() : name_("LQR-based Lateral Controller") {
   ROS_INFO("    registering Lat controller...");
 }
 
@@ -21,16 +22,72 @@ LatController::~LatController() {
 }
 
 Result_state LatController::Init(const ControlConf *control_conf) {
-  control_conf_ = control_conf;
-  if (control_conf_ == nullptr) {
-    controller_initialized_ = false;
-    ROS_ERROR("get_lateral_param() nullptr");
-    return State_Failed;
+  if (!LoadControlConf(control_conf)) {
+    ROS_ERROR("failed to load control conf");
   }
+
+  // Matrix init operations.
+  const int matrix_size = basic_state_size_ + preview_window_;
+  matrix_a_ = Matrix::Zero(basic_state_size_, basic_state_size_);
+  matrix_ad_ = Matrix::Zero(basic_state_size_, basic_state_size_);
+  matrix_adc_ = Matrix::Zero(matrix_size, matrix_size);
+  matrix_a_(0, 1) = 1.0;
+  matrix_a_(1, 2) = (cf_ + cr_) / mass_;
+  matrix_a_(2, 3) = 1.0;
+  matrix_a_(3, 2) = (lf_ * cf_ - lr_ * cr_) / iz_;
+
+  matrix_a_coeff_ = Matrix::Zero(matrix_size, matrix_size);
+  matrix_a_coeff_(1, 1) = -(cf_ + cr_) / mass_;
+  matrix_a_coeff_(1, 3) = (lr_ * cr_ - lf_ * cf_) / mass_;
+  matrix_a_coeff_(2, 3) = 1.0;
+  matrix_a_coeff_(3, 1) = (lr_ * cr_ - lf_ * cf_) / iz_;
+  matrix_a_coeff_(3, 3) = -1.0 * (lf_ * lf_ * cf_ + lr_ * lr_ * cr_) / iz_;
+
+  matrix_b_ = Matrix::Zero(basic_state_size_, 1);
+  matrix_bd_ = Matrix::Zero(basic_state_size_, 1);
+  matrix_bdc_ = Matrix::Zero(matrix_size, 1);
+  matrix_b_(1, 0) = cf_ / mass_;
+  matrix_b_(3, 0) = lf_ * cf_ / iz_;
+  matrix_bd_ = matrix_b_ * ts_;
+
+  matrix_state_ = Matrix::Zero(matrix_size, 1);
+  matrix_k_ = Matrix::Zero(1, matrix_size);
+  matrix_r_ = Matrix::Identity(1, 1);
+  matrix_q_ = Matrix::Zero(matrix_size, matrix_size);
+
+  matrix_q_updated_ = matrix_q_;
   
   auto &lat_controller_conf = control_conf->lat_controller_conf();
 
   return State_Ok;
+}
+
+bool LatController::LoadControlConf(const ControlConf *control_conf) {
+
+  if (!control_conf) {
+    ROS_ERROR("[LatController] control_conf == nullptr");
+    return false;
+  }
+
+  ts_ = control_conf->lat_controller_conf().ts();
+  cf_ = control_conf->lat_controller_conf().cf();
+  cr_ = control_conf->lat_controller_conf().cr();
+  preview_window_ = control_conf->lat_controller_conf().preview_window();
+  const double mass_fl = control_conf->lat_controller_conf().mass_fl();
+  const double mass_fr = control_conf->lat_controller_conf().mass_fr();
+  const double mass_rl = control_conf->lat_controller_conf().mass_rl();
+  const double mass_rr = control_conf->lat_controller_conf().mass_rr();
+  const double mass_front = mass_fl + mass_fr;
+  const double mass_rear = mass_rl + mass_rr;
+
+  lf_ = wheelbase_ * (1.0 - mass_front / mass_);
+  lr_ = wheelbase_ * (1.0 - mass_rear / mass_);
+  iz_ = lf_ * lf_ * mass_front + lr_ * lr_ * mass_rear;
+
+  lqr_eps_ = control_conf->lat_controller_conf().eps();
+  lqr_max_iteration_ = control_conf->lat_controller_conf().max_iteration();
+
+  return true;
 }
 
 std::string LatController::Name() const { return name_; }
--- modules/src/control/src/controller/lat_controller.h
+++ modules/src/control/src/controller/lat_controller.h
@@ -8,6 +8,8 @@
 
 #include "control/src/common/trajectory_analyzer.h"
 
+#include <eigen3/Eigen/Core>
+
 namespace EDrive {
 namespace control {
 
@@ -72,11 +74,75 @@ class LatController : public Controller {
 
  private:
   std::string name_;
-  std::unique_ptr<TrajectoryAnalyzer> trajectory_analyzer_;
   const planning::ADCTrajectory *trajectory_message_ = nullptr;
 
   bool controller_initialized_ = false;
   const ControlConf *control_conf_ = nullptr;
+
+ protected:
+
+  bool LoadControlConf(const ControlConf *control_conf);
+
+  // a proxy to analyze the planning trajectory
+  std::unique_ptr<TrajectoryAnalyzer> trajectory_analyzer_;
+
+  // the following parameters are vehicle physics related.
+  // control time interval
+  double ts_ = 0.0;
+  // corner stiffness; front
+  double cf_ = 0.0;
+  // corner stiffness; rear
+  double cr_ = 0.0;
+  // distance between front and rear wheel center
+  double wheelbase_ = 0.0;
+  // mass of the vehicle
+  double mass_ = 0.0;
+  // distance from front wheel center to COM
+  double lf_ = 0.0;
+  // distance from rear wheel center to COM
+  double lr_ = 0.0;
+  // rotational inertia
+  double iz_ = 0.0;
+  // the ratio between the turn of the steering wheel and the turn of the wheels
+  double steer_transmission_ratio_ = 0.0;
+  // the maximum turn of steer
+  double steer_single_direction_max_degree_ = 0.0;
+
+  // number of control cycles look ahead (preview controller)
+  int preview_window_ = 0;
+  // number of states without previews, includes
+  // lateral error, lateral error rate, heading error, heading error rate
+  const int basic_state_size_ = 4;
+  // vehicle state matrix
+  Eigen::MatrixXd matrix_a_;
+  // vehicle state matrix (discrete-time)
+  Eigen::MatrixXd matrix_ad_;
+  // vehicle state matrix compound; related to preview
+  Eigen::MatrixXd matrix_adc_;
+  // control matrix
+  Eigen::MatrixXd matrix_b_;
+  // control matrix (discrete-time)
+  Eigen::MatrixXd matrix_bd_;
+  // control matrix compound
+  Eigen::MatrixXd matrix_bdc_;
+  // gain matrix
+  Eigen::MatrixXd matrix_k_;
+  // control authority weighting matrix
+  Eigen::MatrixXd matrix_r_;
+  // state weighting matrix
+  Eigen::MatrixXd matrix_q_;
+  // updated state weighting matrix
+  Eigen::MatrixXd matrix_q_updated_;
+  // vehicle state matrix coefficients
+  Eigen::MatrixXd matrix_a_coeff_;
+  // 4 by 1 matrix; state matrix
+  Eigen::MatrixXd matrix_state_;
+
+  // parameters for lqr solver; number of iterations
+  int lqr_max_iteration_ = 0;
+  // parameters for lqr solver; threshold for computation
+  double lqr_eps_ = 0.0;
+
 };
 
 } // namespace control
