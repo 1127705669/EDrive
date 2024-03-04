f3c26616c6ab3fc27be51f709f0eb59f728110f3
ethan <ethan.liqi.yi@gmail.com>
2024-03-04 11:44:23 +0800

    fix(viewer): fix path publisher
    Change-Id: If6c4886005b72f15f719211155005873939c0415

--- modules/src/viewer/src/visual_component/planning/planning_handle.cc
+++ modules/src/viewer/src/visual_component/planning/planning_handle.cc
@@ -20,6 +20,31 @@ EDrive::Result_state Planning_handle::Init(const ViewerConf *viewer_conf) {
 }
 
 EDrive::Result_state Planning_handle::InterfaceMatch() {
+
+  trajectory_path_->header.frame_id = "map";
+
+  for (auto point : trajectory_->trajectory_point)
+  {
+    geometry_msgs::PoseStamped pose_stamped;
+
+    // Set the timestamp and coordinate frame of the point
+    pose_stamped.header.stamp = ros::Time::now();
+    pose_stamped.header.frame_id = "map";
+
+    // Set point location
+    pose_stamped.pose.position.x = point.path_point.x;
+    pose_stamped.pose.position.y = point.path_point.y;
+    pose_stamped.pose.position.z = point.path_point.z;
+
+    // Set the direction of the point
+    pose_stamped.pose.orientation.x = 0.0;
+    pose_stamped.pose.orientation.y = 0.0;
+    pose_stamped.pose.orientation.z = 0.0;
+    pose_stamped.pose.orientation.w = 1.0;
+
+    // Add this point to the path
+    trajectory_path_->poses.push_back(pose_stamped);
+  }
   return State_Ok;
 }
 
