45f1fdf0b90fa78e4d8d98b44ed4213c2501db61
ethan <ethan.liqi.yi@gmail.com>
2024-06-19 22:10:07 +0800

    feat(common): add mpc solover
    Change-Id: Ic325220a2aabf5a549e8f3a9b38060f4efc6b2de

---
+++ modules/src/common/src/math/qp_solver/qp_solver.h
@@ -0,0 +1,59 @@
+/******************************************************************************
+ * Copyright 2024 The EDrive Authors. All Rights Reserved.
+ *****************************************************************************/
+
+/**
+ * @file: qp_solver.h
+ * @brief: quadratic programming base class
+ *
+ *        min_x  : q(x) = 0.5 * x^T * Q * x  + x^T c
+ *        with respect to:  A * x = b (equality constraint)
+ *                          C * x >= d (inequality constraint)
+ **/
+
+#pragma once
+
+#include "eigen3/Eigen/Core"
+#include "eigen3/Eigen/LU"
+
+namespace EDrive {
+namespace common {
+namespace math {
+
+class QpSolver {
+ public:
+  QpSolver(const Eigen::MatrixXd& kernel_matrix, const Eigen::MatrixXd& offset,
+           const Eigen::MatrixXd& affine_inequality_matrix,
+           const Eigen::MatrixXd& affine_inequality_boundary,
+           const Eigen::MatrixXd& affine_equality_matrix,
+           const Eigen::MatrixXd& affine_equality_boundary);
+  virtual ~QpSolver() = default;
+
+  virtual void set_pos_semi_definite_hessian() {}
+  virtual void set_pos_definite_hessian() {}
+  virtual void EnableCholeskyRefactorisation(const int) {}
+  virtual void SetTerminationTolerance(const double) {}
+  virtual bool Solve() = 0;
+
+  const Eigen::MatrixXd& params() const;
+  const Eigen::MatrixXd& kernel_matrix() const;
+  const Eigen::MatrixXd& offset() const;
+  const Eigen::MatrixXd& affine_equality_matrix() const;
+  const Eigen::MatrixXd& affine_equality_boundary() const;
+  const Eigen::MatrixXd& affine_inequality_matrix() const;
+  const Eigen::MatrixXd& affine_inequality_boundary() const;
+
+ protected:
+  virtual bool sanity_check() = 0;
+  Eigen::MatrixXd params_;
+  Eigen::MatrixXd kernel_matrix_;
+  Eigen::MatrixXd offset_;
+  Eigen::MatrixXd affine_inequality_matrix_;
+  Eigen::MatrixXd affine_inequality_boundary_;
+  Eigen::MatrixXd affine_equality_matrix_;
+  Eigen::MatrixXd affine_equality_boundary_;
+};
+
+}  // namespace math
+}  // namespace common
+}  // namespace EDrive
